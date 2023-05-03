fc8bd0e57393973a5f8b5009f99c21843795d624
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-05-03 17:19:43 +0600

    sub component created and footer completed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOxrPwIKkcyKvZLQMaLcck6Mx9X8t0Ex4",
+  authDomain: "shaderkhabar-a10.firebaseapp.com",
+  projectId: "shaderkhabar-a10",
+  storageBucket: "shaderkhabar-a10.appspot.com",
+  messagingSenderId: "1011879357595",
+  appId: "1:1011879357595:web:462431b33ad86c4d99aa3e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../pages/Shared/Header/Header';
+import Footer from '../pages/Shared/Footer/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default Main;
